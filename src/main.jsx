72ddf9329dedd94d55b90cdf145375f832d5ae92
AssafBenIshay <b.i.app.solutions@gmail.com>
2025-05-31 08:01:53 +0300

    footer added2

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <small>All rights reserved to <a href='https://github.com/AssafBenIshay'><b>Assaf Ben-Ishay</b></a> 2025</small>
+    <small>All rights reserved to <a href='https://github.com/AssafBenIshay/kawaii2'><b>Assaf Ben-Ishay</b></a> 2025.</small>
 
   </StrictMode>,
 )
