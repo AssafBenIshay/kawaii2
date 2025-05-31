cd091fce8b03987b673acc29ce2faa53de2977e5
AssafBenIshay <b.i.app.solutions@gmail.com>
2025-05-31 08:17:14 +0300

    subcomponentcss fixed

--- src/components/GameAlertModel.jsx
+++ src/components/GameAlertModel.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../components/subComponents/subComponentsCss/subComponents.css'
+import './subComponents/subComponentsCss/subComponents.css'
 import ColorHeader from './subComponents/ColorHeader';
 
 export default function GameAlertModel({setGameAlert,gameAlert}) {
